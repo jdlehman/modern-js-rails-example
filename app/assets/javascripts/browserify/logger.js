7cf404acb4e8f1cb2895dd8dc01cf7d4944f97ab
Jonathan Lehman <jonathan.lehman91@gmail.com>
2016-04-29 14:44:10 -0400

    We can also import node_modules

--- app/assets/javascripts/browserify/logger.js
+++ app/assets/javascripts/browserify/logger.js
@@ -1,4 +1,5 @@
 import Dog from './dog';
+import uuid from 'node-uuid';
 
 const dog = new Dog({name: 'Spot', sound: 'BARK! WOOF!'});
 
@@ -7,4 +8,7 @@ console.log(
 );
 console.log(
   dog.emitSound()
+);
+console.log(
+  uuid.v4()
 );
